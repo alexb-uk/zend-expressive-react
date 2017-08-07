0a51f83489036e42304554940d946d54ccc76694
abarnes <alex.barnes@completeautomotivesolutions.co.uk>
2017-08-07 12:31:35 +0100

    Testing click events using Enzyme

--- test/AppTest/js/hworld-enzyme.test.jsx
+++ test/AppTest/js/hworld-enzyme.test.jsx
@@ -7,17 +7,32 @@ import Helloworld from "../../../src/App/js/components/helloworld";
 
 describe("<Helloworld />", () => {
   it("renders welcome <h1> component", () => {
-    const wrapper = shallow(<Helloworld date={new Date("2001/02/03 01:20:30")} />);
+    const wrapper = shallow(<Helloworld date={new Date()} />);
     const welcome = <h1>Hello, world!</h1>;
 
     expect(wrapper).toContainReact(welcome);
   });
 
   it("renders time <h2> component", () => {
-    const testDate = new Date("1999/02/03 01:20:30");
+    const testDate = new Date("2001/02/03 01:20:30");
     const wrapper  = shallow(<Helloworld date={testDate} />);
     const time     = <h2>It is {testDate.toLocaleTimeString()} precisely!</h2>;
 
     expect(wrapper).toContainReact(time);
+  });
+
+  it("Like counter increases", () => {
+    const wrapper = shallow(<Helloworld date={new Date()} />);
+    const likeButton = wrapper.find("button");
+
+    // Initial state
+    expect(wrapper.find("span").text()).toEqual("0");
+
+    likeButton.simulate("click");
+    expect(wrapper.find("span").text()).toEqual("1");
+
+    likeButton.simulate("click");
+    expect(wrapper.find("span").text()).toEqual("2");
+
   });
 });
