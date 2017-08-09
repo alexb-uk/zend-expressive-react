da770bd36485cf4a2e09f54227beb7d4c0bfb154
abarnes <alex.barnes@completeautomotivesolutions.co.uk>
2017-08-09 15:55:44 +0100

    Extending tests and added win condition

--- test/AppTest/js/board.test.jsx
+++ test/AppTest/js/board.test.jsx
@@ -30,6 +30,58 @@ describe("<Board />", () => {
         expect(tree).toMatchSnapshot();
     });
 
+    // Snapshot testing complete game
+    it("Player X wins game", () => {
+        const component = renderer.create(
+            <Board/>
+        );
+        let tree = component.toJSON();
+
+        tree.children[1].children[0].props.onClick();
+        tree.children[2].children[0].props.onClick();
+        tree.children[1].children[1].props.onClick();
+
+        // re-rendering
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+
+        tree.children[2].children[1].props.onClick();
+        tree.children[1].children[2].props.onClick();
+
+        // re-rendering
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+
+        tree.children[2].children[2].props.onClick();
+        // re-rendering
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    // Snapshot testing complete game
+    it("Player O wins game", () => {
+        const component = renderer.create(
+            <Board/>
+        );
+        let tree = component.toJSON();
+
+        tree.children[1].children[0].props.onClick();
+        tree.children[2].children[0].props.onClick();
+        tree.children[1].children[1].props.onClick();
+
+        // re-rendering
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+
+        tree.children[2].children[1].props.onClick();
+        tree.children[3].children[0].props.onClick();
+        tree.children[2].children[2].props.onClick();
+
+        // re-rendering
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
     // Enzyme testing
     it("<Board /> component handles clicks - Enzyme", () => {
         const wrapper = shallow(<Board />);
@@ -39,11 +91,31 @@ describe("<Board />", () => {
         expect(wrapper.state().squares[0]).toBe("X");
         expect(wrapper.state().squares[1]).toBe(null);
 
+        expect(wrapper.find(".status").text()).toEqual("Next player: O");
+
         const secondSquare  = wrapper.find(Square).at(1);
 
         secondSquare.simulate("click");
         expect(wrapper.state().squares[0]).toBe("X");
         expect(wrapper.state().squares[1]).toBe("O");
         expect(wrapper.state().squares[2]).toBe(null);
+
+        expect(wrapper.find(".status").text()).toEqual("Next player: X");
+    });
+
+    // Enzyme testing complete game
+    it("Player O wins game - Enzyme", () => {
+        const wrapper = shallow(<Board />);
+
+        expect(wrapper.find(".status").text()).toEqual("Next player: X");
+
+        wrapper.find(Square).at(0).simulate("click");
+        wrapper.find(Square).at(3).simulate("click");
+        wrapper.find(Square).at(1).simulate("click");
+        wrapper.find(Square).at(4).simulate("click");
+        wrapper.find(Square).at(8).simulate("click");
+        wrapper.find(Square).at(5).simulate("click");
+
+        expect(wrapper.find(".status").text()).toEqual("Winner: O");
     });
 });
