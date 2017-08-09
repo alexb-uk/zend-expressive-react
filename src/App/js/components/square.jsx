c9d4a2f9cef463dc90357000a2070e6c2b58eba5
abarnes <alex.barnes@completeautomotivesolutions.co.uk>
2017-08-09 15:12:33 +0100

    Convert Square from React class to Functional Component

--- src/App/js/components/square.jsx
+++ src/App/js/components/square.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
-}
+const Square = ({value, onClick}) => {
+    return (
+        <button className="square" onClick={() => onClick()}>
+            {value}
+        </button>
+    );
+};
 
 Square.propTypes = {
     onClick: PropTypes.func.isRequired,
     value: PropTypes.string
 };
+
+export default Square;
