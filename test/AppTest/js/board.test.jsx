27dffcd9be0fec24dd3cdeed761eb77c552875d8
abarnes <alex.barnes@completeautomotivesolutions.co.uk>
2017-08-10 10:02:46 +0100

    Start of migrating logic up to Game from Board. Game tests are passing but Board currently failing.

--- test/AppTest/js/board.test.jsx
+++ test/AppTest/js/board.test.jsx
@@ -6,34 +6,28 @@ import Board from "../../../src/App/js/components/board";
 import Square from "../../../src/App/js/components/square";
 
 describe("<Board />", () => {
+    const emptySquares = new Array(9).fill(null);
+
     it("renders <Board /> component", () => {
+        const myMock = jest.fn();
         const component = renderer.create(
-            <Board/>
+            <Board
+                squares={emptySquares}
+                onClick={(i) => myMock(i)}
+            />
         );
         let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
-
-    // Snapshot testing
-    it("<Board /> component handles clicks", () => {
-        const component = renderer.create(
-            <Board/>
-        );
-        let tree = component.toJSON();
-
-        // Click first row, second column
-        let link = tree.children[1].children[1];
-        link.props.onClick();
-
-        // re-rendering
-        tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     // Snapshot testing complete game
     it("Player X wins game", () => {
+        const myMock = jest.fn();
         const component = renderer.create(
-            <Board/>
+            <Board
+                squares={emptySquares}
+                onClick={(i) => myMock(i)}
+            />
         );
         let tree = component.toJSON();
 
@@ -60,8 +54,12 @@ describe("<Board />", () => {
 
     // Snapshot testing complete game
     it("Player O wins game", () => {
+        const myMock = jest.fn();
         const component = renderer.create(
-            <Board/>
+            <Board
+                squares={emptySquares}
+                onClick={(i) => myMock(i)}
+            />
         );
         let tree = component.toJSON();
 
@@ -84,23 +82,21 @@ describe("<Board />", () => {
 
     // Enzyme testing
     it("<Board /> component handles clicks - Enzyme", () => {
-        const wrapper = shallow(<Board />);
-        const square  = wrapper.find(Square).first();
-
+        const myMock  = jest.fn();
+        const squares = new Array(9).fill(null);
+        const wrapper = shallow(
+            <Board
+                squares={squares}
+                onClick={(i) => myMock(i)}
+            />
+        );
+        const square = wrapper.find(Square).first();
         square.simulate("click");
-        expect(wrapper.state().squares[0]).toBe("X");
-        expect(wrapper.state().squares[1]).toBe(null);
-
-        expect(wrapper.find(".status").text()).toEqual("Next player: O");
+        expect(myMock.mock.calls.length).toBe(1);
 
         const secondSquare  = wrapper.find(Square).at(1);
-
         secondSquare.simulate("click");
-        expect(wrapper.state().squares[0]).toBe("X");
-        expect(wrapper.state().squares[1]).toBe("O");
-        expect(wrapper.state().squares[2]).toBe(null);
-
-        expect(wrapper.find(".status").text()).toEqual("Next player: X");
+        expect(myMock.mock.calls.length).toBe(2);
     });
 
     // Enzyme testing complete game
--- test/AppTest/js/game.test.jsx
+++ test/AppTest/js/game.test.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+
 import Game from "../../../src/App/js/components/game";
 
 describe("<Game />", () => {
-  it("renders <Game /> component", () => {
-    const component = renderer.create(
-      <Game />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+    it("renders <Game /> component", () => {
+        const component = renderer.create(
+            <Game />
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    // Enzyme testing complete game
+    it("Player O wins game - Enzyme", () => {
+        const wrapper = shallow(<Game />);
+
+        expect(wrapper.find(".game-info").text()).toEqual("Next player: X");
+
+        wrapper.instance().handleClick(0);
+
+        expect(wrapper.find(".game-info").text()).toEqual("Next player: O");
+        let lastHistory = wrapper.state("history").slice(-1)[0]["squares"];
+        expect(lastHistory[0]).toEqual("X");
+
+        wrapper.instance().handleClick(3);
+
+        expect(wrapper.find(".game-info").text()).toEqual("Next player: X");
+        lastHistory = wrapper.state("history").slice(-1)[0]["squares"];
+        expect(lastHistory[3]).toEqual("O");
+
+        wrapper.instance().handleClick(1);
+        wrapper.instance().handleClick(4);
+        wrapper.instance().handleClick(8);
+        wrapper.instance().handleClick(5);
+
+        expect(wrapper.find(".game-info").text()).toEqual("Winner: O");
+    });
+
 });
